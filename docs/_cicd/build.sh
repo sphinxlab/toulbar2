7fa1f2f81da021c9334257a58fdff0d4913d79f4
Nathalie Rousse <nathalie.rousse@inra.fr>
2022-02-08 10:54:38 +0100

    [docs] cicd files

---
+++ docs/_cicd/build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -x
+
+# build.sh
+
+# Builds the documentation for one branch
+
+# .github/workflows/docs-deploy.yml |-> cicd.sh ->| deploy.sh |-> build.sh
+# .github/workflows/docs-devel.yml  |             | devel.sh  |
+ 
+docroot=$1
+current_version=$2
+
+###############################################################################
+
+./clean # cleanup
+#make -C docs clean
+
+export current_version # for conf.py
+ 
+echo "INFO: Building sites for ${current_version}"
+ 
+if [ ! -e 'docs/conf.py' ]; then
+    echo -e "\tINFO: Can not find 'docs/conf.py' (skipped)"
+    continue
+fi
+
+# Build doxygen docs (xml used by breathe)
+mkdir build
+pushd build 
+#cmake -DBUILD_API_DOC=ON -DBUILD_API_DOC_LATEX=ON ..
+cmake -DBUILD_API_DOC=ON ..
+make doc #make (for help ?)
+popd
+ 
+languages="en `find docs/locales/ -mindepth 1 -maxdepth 1 -type d -exec basename '{}' \;`"
+    for current_language in ${languages}; do
+ 
+      export current_language # for conf.py
+ 
+      echo "INFO: Building for ${current_language}"
+ 
+      # html
+      sphinx-build -b html docs/ docs/_build/html/${current_language}/${current_version} -D language="${current_language}"
+ 
+      # pdf
+      sphinx-build -b rinoh docs/ docs/_build/rinoh -D language="${current_language}"
+      mkdir -p "${docroot}/${current_language}/${current_version}"
+      cp "docs/_build/rinoh/toulbar2.pdf" "${docroot}/${current_language}/${current_version}/toulbar2_${current_language}_${current_version}.pdf"
+
+      # epub
+      sphinx-build -b epub docs/ docs/_build/epub -D language="${current_language}"
+      mkdir -p "${docroot}/${current_language}/${current_version}"
+      cp "docs/_build/epub/toulbar2.epub" "${docroot}/${current_language}/${current_version}/toulbar2_${current_language}_${current_version}.epub"
+ 
+      # copy into docroot the static assets produced by the above build
+      rsync -av "docs/_build/html/" "${docroot}/"
+ 
+    done
+
+exit 0
+
---
+++ docs/_cicd/cicd.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+set -x
+
+# cicd.sh
+
+# Builds the documentation using sphinx (and doxygen + breathe for C++ code)
+# and updates GitHub Pages.
+ 
+# .github/workflows/docs-deploy.yml |-> cicd.sh ->| deploy.sh |-> build.sh
+# .github/workflows/docs-devel.yml  |             | devel.sh  |
+
+# usecase available values : "deploy" or "devel"
+usecase=$1
+
+###############################################################################
+# Install depends
+ 
+# required for tzdata
+TZ=Europe/Paris
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+apt-get update -y
+apt-get -y install git rsync python3-stemmer python3-git python3-pip
+ 
+# Required to build doxygen docs (xml used by breathe)
+apt-get -y install cmake g++ libgmp-dev libboost-graph-dev libboost-iostreams-dev zlib1g-dev liblzma-dev libxml2-dev libopenmpi-dev libboost-mpi-dev libjemalloc-dev pkg-config texlive-latex-recommended  texlive-fonts-recommended doxygen
+
+python3 -m pip install --upgrade sphinx sphinx_rtd_theme rinohtype pygments breathe
+
+###############################################################################
+# Declare variables
+ 
+pwd
+ls -lah
+export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
+ 
+# make a new temp dir which will be our GitHub Pages docroot
+docroot=`mktemp -d`
+
+export REPO_NAME="${GITHUB_REPOSITORY##*/}"
+ 
+###############################################################################
+# Build docs
+
+if [ ${usecase} == "devel" ]; then
+    docs/_cicd/devel.sh ${docroot}
+else
+    docs/_cicd/deploy.sh ${docroot}
+fi
+ 
+###############################################################################
+# Update GitHub Pages
+ 
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+ 
+pushd "${docroot}"
+ 
+# don't bother maintaining history; just generate fresh
+git init
+git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git checkout -b gh-pages
+ 
+# add .nojekyll to the root so that github won't 404 on content added to dirs
+# that start with an underscore (_), such as our "_content" dir..
+touch .nojekyll
+ 
+if [ ${usecase} == "devel" ]; then
+
+# add devel.html
+cat > devel.html <<EOF
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>toulbar2 docs (by devel.sh) </title>
+  </head>
+  <body>
+    <p> ... language/branchname ...</p>
+  </body>
+</html>
+
+EOF
+
+else
+
+# add index.html (redirection to default docs language/version)
+cat > index.html <<EOF
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>toulbar2 docs (by deploy.sh) </title>
+    <meta http-equiv = "refresh" content="0; url='/${REPO_NAME}/en/master/'" />
+  </head>
+  <body>
+    <p>Redirection to <a href="/${REPO_NAME}/en/master/">toulbar2 main documentation</a>...</p>
+  </body>
+</html>
+EOF
+
+fi
+ 
+# add README.md
+cat > README.md <<EOF
+# GitHub Pages Cache
+
+Branch not intended to be viewed on github.com, for more see docs/README.md.
+
+EOF
+ 
+git add .
+ 
+msg="Updating Docs for commit ${GITHUB_SHA} made on `date -d"@${SOURCE_DATE_EPOCH}" --iso-8601=seconds` from ${GITHUB_REF} by ${GITHUB_ACTOR}"
+git commit -am "${msg}"
+ 
+git push deploy gh-pages --force
+ 
+popd
+ 
+exit 0
+
