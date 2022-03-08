573e4ad7161cfce8959967c7ff7d207781682282
Nathalie Rousse <nathalie.rousse@inra.fr>
2022-03-08 13:18:35 +0100

    [docs] cicd for devel case

--- docs/_cicd/build.sh
+++ docs/_cicd/build.sh
@@ -6,77 +6,51 @@ set -x
 
 # Builds the documentation for one branch
 
-# .github/workflows/docs-deploy.yml -> "deploy.sh"       -> build.sh
-# .github/workflows/docs-devel.yml  -> "deploy.sh devel" -> build.sh           
+# .github/workflows/docs-devel.yml -> devel.sh -> build.sh           
 
 docroot=$1
 current_version=$2
-
-###############################################################################
 
 ./clean # cleanup
 #make -C docs clean
 
 export current_version # for conf.py
  
-echo "INFO: Building sites for ${current_version}"
+echo "INFO: Building for ${current_version}"
  
 if [ ! -e 'docs/conf.py' ]; then
     echo -e "\tINFO: Can not find 'docs/conf.py' (skipped)"
     continue
 fi
 
+# Install Sphinx
+python -m pip install --upgrade pip
+pip install -r docs/_local/requirement.txt
+
 # Build doxygen docs (xml used by breathe)
 mkdir build
 pushd build 
-#cmake -DBUILD_API_DOC=ON -DBUILD_API_DOC_LATEX=ON ..
 cmake -DBUILD_API_DOC=ON ..
-make doc #make (for help ?)
+make doc
 popd
  
-languages="en `find docs/locales/ -mindepth 1 -maxdepth 1 -type d -exec basename '{}' \;`"
-    for current_language in ${languages}; do
- 
-      export current_language # for conf.py
- 
-      echo "INFO: Building for ${current_language}"
- 
-      # 1. pdf before html
-      rm -f docs/_files/*.*
-      cp README.md docs/_files/.
-      pushd docs
-      make latexpdf
-      echo "1. ls -l _build/latex"
-      ls -l _build/latex      
-      popd
-      cp docs/_build/latex/*.pdf docs/_files/.
+# Build Sphinx docs
 
-      # html
-      sphinx-build -b html docs/ docs/_build/html/${current_language}/${current_version} -D language="${current_language}"
+pushd docs
+make docs
+popd
+
+# epub
+sphinx-build -b epub docs/ docs/_build/epub
+mkdir -p "${docroot}/${current_version}"
+cp "docs/_build/epub/toulbar2.epub" "${docroot}/${current_version}/toulbar2_${current_version}.epub"
  
-      # 2. pdf after html
-      rm -f docs/_files/*.*
-      cp README.md docs/_files/.
-      pushd docs
-      make latexpdf
-      echo "2. ls -l _build/latex"
-      ls -l _build/latex      
-      popd
-      cp docs/_build/latex/*.pdf docs/_files/.
-
-      # epub
-      sphinx-build -b epub docs/ docs/_build/epub -D language="${current_language}"
-      mkdir -p "${docroot}/${current_language}/${current_version}"
-      cp "docs/_build/epub/toulbar2.epub" "${docroot}/${current_language}/${current_version}/toulbar2_${current_language}_${current_version}.epub"
+# copy into docroot the static assets produced by the above build
+rsync -av "docs/_build/html/" "${docroot}/"
  
-      # copy into docroot the static assets produced by the above build
-      rsync -av "docs/_build/html/" "${docroot}/"
-
-      # clean
-      cp -f docs/_files/tmp/*.* docs/_files/.
-      rm -fr docs/_files/tmp
-
-    done
+# clean
+cp -f docs/_files/tmp/*.* docs/_files/.
+rm -fr docs/_files/tmp
 
 exit 0
 
--- docs/_cicd/devel.sh
+++ docs/_cicd/devel.sh
@@ -2,17 +2,12 @@
 
 set -x
 
-# deploy.sh
+# devel.sh
 
 # Builds the documentation using sphinx (and doxygen + breathe for C++ code)
 # and updates GitHub Pages.
-# In deployment case by default, in development case if INPUT "devel".
- 
-# .github/workflows/docs-deploy.yml -> "deploy.sh"       -> build.sh
-# .github/workflows/docs-devel.yml  -> "deploy.sh devel" -> build.sh           
 
-# usecase available values : "devel" or none
-usecase=$1
+# .github/workflows/docs-devel.yml  -> devel.sh -> build.sh           
 
 ###############################################################################
 # Install depends
@@ -25,10 +20,7 @@ apt-get update -y
 apt-get -y install git rsync python3-stemmer python3-git python3-pip
  
 # Required to build doxygen docs (xml used by breathe)
-#apt-get -y install cmake g++ libgmp-dev libboost-graph-dev libboost-iostreams-dev zlib1g-dev liblzma-dev libxml2-dev libopenmpi-dev libboost-mpi-dev libjemalloc-dev pkg-config texlive-latex-recommended  texlive-fonts-recommended graphviz doxygen
 apt-get -y install cmake g++ libgmp-dev libboost-graph-dev libboost-iostreams-dev zlib1g-dev liblzma-dev libxml2-dev libopenmpi-dev libboost-mpi-dev libjemalloc-dev pkg-config texlive-latex-base texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended latexmk graphviz doxygen
-
-python3 -m pip install --upgrade sphinx sphinx_rtd_theme pygments breathe recommonmark
 
 ###############################################################################
 # Declare variables
@@ -45,30 +37,20 @@ export REPO_NAME="${GITHUB_REPOSITORY##*/}"
 ###############################################################################
 # Build docs
 
-if [[ ${usecase} == "devel" ]]; then
+# get a list of branches, excluding 'HEAD' and 'gh-pages'
+#versions="`git for-each-ref '--format=%(refname:lstrip=-1)' refs/remotes/origin/ | grep -viE '^(HEAD|gh-pages)$'`"
 
-    # get a list of branches, excluding 'HEAD' and 'gh-pages'
-    #versions="`git for-each-ref '--format=%(refname:lstrip=-1)' refs/remotes/origin/ | grep -viE '^(HEAD|gh-pages)$'`"
+# manual selection of a list of branches (at least master !)
+###versions="master sphinx-nr sphinx-usr"
+#versions="sphinx-nr step1 step2"
+versions="sphinx-nr"
 
-    # manual selection of a list of branches (at least master !)
-    ###versions="master sphinx-nr sphinx-usr"
-    #versions="sphinx-nr step1 step2"
-    versions="sphinx-nr"
+for current_version in ${versions}; do
+   git checkout ${current_version}
+   docs/_cicd/build.sh ${docroot} ${current_version}
+   git checkout master # return to master branch
+done
 
-    for current_version in ${versions}; do
-       git checkout ${current_version}
-       docs/_cicd/build.sh ${docroot} ${current_version}
-       git checkout master # return to master branch
-    done
-
-else
-    # main master branch
-    current_version="master" 
-    git checkout ${current_version}
-    docs/_cicd/build.sh ${docroot} ${current_version}
-    git checkout master # return to master branch
-fi
- 
 ###############################################################################
 # Update GitHub Pages
  
@@ -86,8 +68,6 @@ git checkout -b gh-pages
 # that start with an underscore (_), such as our "_content" dir..
 touch .nojekyll
  
-if [[ ${usecase} == "devel" ]]; then
-
 # add devel.html
 cat > devel.html <<EOF
 <!DOCTYPE html>
@@ -102,24 +82,6 @@ cat > devel.html <<EOF
 
 EOF
 
-else
-
-# add index.html (redirection to default docs language/version)
-cat > index.html <<EOF
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>toulbar2 docs (by deploy.sh) </title>
-    <meta http-equiv = "refresh" content="0; url='/${REPO_NAME}/en/master/'" />
-  </head>
-  <body>
-    <p>Redirection to <a href="/${REPO_NAME}/en/master/">toulbar2 main documentation</a>...</p>
-  </body>
-</html>
-EOF
-
-fi
- 
 # add README.md
 cat > README.md <<EOF
 # GitHub Pages Cache
